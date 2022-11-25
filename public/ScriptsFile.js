d9553f0f71aea81bd9280371e4cd2e14da12114c
Bernat Jara Ortínez <bernat.jara@estudiantat.upc.edu>
2022-11-25 14:00:01 +0100

    Registro web funcionando

---
+++ public/ScriptsFile.js
@@ -0,0 +1,26 @@
+let URL='http://localhost:8080';
+function signup() {
+
+    var email = $('#email').val();
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    $.ajax({
+        contentType: "application/json",
+        type: 'POST',
+        url: URL+'/dsaApp/user/signUp',
+        data: JSON.stringify({ "email": email,"username": username, "password": password}),
+        dataType: 'json',
+        success: function (result) {
+            alert("Sign up completed. Nice to meet you, " + username);
+            localStorage.setItem("activeUser", username);
+        },
+        error: function (error) {
+            console.log(error);
+            if (email == null || username == null || password == null)
+                alert("You left something blank. Please try again!");
+            else
+                alert("Username or email already in use. Please try again!");
+        }
+    });
+}
