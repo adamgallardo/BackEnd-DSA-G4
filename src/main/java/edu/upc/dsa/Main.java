66c86dd30559f3b16ddf23ed07b1391e024684e7
omarferrer30 <92688337+omarferrer30@users.noreply.github.com>
2023-01-08 13:27:34 +0100

    preparado para entorno de producción

--- src/main/java/edu/upc/dsa/Main.java
+++ src/main/java/edu/upc/dsa/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         BeanConfig beanConfig = new BeanConfig();
 
-        beanConfig.setHost("localhost:8080"); //147.83.7.206:8080  localhost:8080
+        beanConfig.setHost("147.83.7.206:8080"); //147.83.7.206:8080  localhost:8080
         beanConfig.setBasePath("/dsaApp");
         beanConfig.setContact("support@example.com");
         beanConfig.setDescription("REST API for GameManager");
